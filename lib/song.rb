9407751c72ff2a30fd3ffcf119ff6c35ce6d2987
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-24 02:48:01 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song 
+  
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  attr_accessor :name, :artist, :genre 
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    
+    @@count += 1 
+    @@genres << @genre 
+    @@artists << @artist 
+    
+  end
+  
+  def self.count
+    @@count
+  end
+  def self.genres
+    @@genres 
+  end
+  def self.artists 
+    @@artists
+  end 
+  
+  def self.genre_count 
+    genre_count = {@genre => 
+  end
+end
